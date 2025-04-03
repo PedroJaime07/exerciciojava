8be35d65c326a7f0cd9324cef0ef7d5c7fbef39b
PedroJaime07 <pejotadev07@outlook.com>
2025-04-03 16:01:42 -0300

    Calculadora simples de emprestimo

---
+++ src/ExercicioIntrodutorio/CalcularJuros.java
@@ -0,0 +1,34 @@
+package ExercicioIntrodutorio;
+
+import java.util.Scanner;
+
+public class CalcularJuros {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o valor do emprestimo");
+        float valorEmprestimo = scanner.nextFloat();
+
+        System.out.println("Quantas parcelas:");
+        int parcelas = scanner.nextInt();
+
+        float[] valor = new float[parcelas];
+
+
+        System.out.println("Digite os valores, pode usar virgula");
+        for (int i = 0; i < valor.length; i++) {
+            System.out.println((i + 1) + ": ");
+            valor[i] = scanner.nextFloat();
+
+        }
+
+        float soma = 0;
+        for (float i : valor) {
+            soma += i;
+        }
+
+        System.out.println("Valor do Emprestimo R$" + valorEmprestimo);
+        System.out.println("Valor pago " + soma);
+        System.out.println("Juros: R$" + (soma - valorEmprestimo));
+    }
+}
